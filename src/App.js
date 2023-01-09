7b34465183942bd9488b54e27f0e41deb428125e
chad hinds <chadrickj8@gmail.com>
2023-01-09 15:10:19 -0500

    Add error message

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ function App() {
   const session = useSession(); // tokens, when a session exists, we have a user
   const supabase = useSupabaseClient(); // links up to supabase
 
+  async function googleSignIn() {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        scopes: 'https://www.googleapis.com/auth/calendar'
+      }
+    })
+    if (error) {
+      alert("There was an error logging into Google provider with Supabase")
+    }
+  }
+
   return (
     <div className="App">
 
