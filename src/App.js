b1147127a458c7257fcb2d4e4e9b94d51122ff53
chad hinds <chadrickj8@gmail.com>
2023-01-09 15:04:35 -0500

    Link up to supabase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
 
 function App() {
+  const session = useSession(); // tokens, when a session exists, we have a user
+  const supabase = useSupabaseClient(); // links up to supabase
+
   return (
     <div className="App">
 
