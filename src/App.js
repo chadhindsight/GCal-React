54c2a1f5bc498532de517fb68a14e98cce08e042
chad hinds <chadrickj8@gmail.com>
2023-01-10 13:24:24 -0500

    Add buttons for signing in/ out

--- src/App.js
+++ src/App.js
@@ -13,13 +13,30 @@ function App() {
       }
     })
     if (error) {
+      console.log(error);
       alert("There was an error logging into Google provider with Supabase")
     }
   }
+  async function googleSignOut() {
+    await supabase.auth.signOut()
+  }
+
+  console.log(session)
 
   return (
     <div className="App">
-
+      <div style={{ width: "400px", margin: "30px auto" }}>
+        {
+          session ?
+            <>
+              <h2>Hi there {session.user.email}</h2>
+              <button onClick={() => googleSignOut()}>Sign In With Google</button>
+            </> :
+            <>
+              <button onClick={() => googleSignIn()}>Sign In With Google</button>
+            </>
+        }
+      </div>
     </div>
   );
 }
