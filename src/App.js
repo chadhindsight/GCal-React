27f7eb03c3b4b1b8b93d31dd2eec0ff67643bfb7
chad hinds <chadrickj8@gmail.com>
2023-01-27 17:02:22 -0500

    Remove comment

--- src/App.js
+++ src/App.js
@@ -10,9 +10,8 @@ function App() {
   const [eventName, setEventName] = useState(new Date());
   const [eventDescription, setEventDescription] = useState(new Date());
 
-  const session = useSession(); // when a session exists, we have a user
-  const supabase = useSupabaseClient(); // links up to supabase 
-
+  const session = useSession(); // when a session exists, we have a user.
+  const supabase = useSupabaseClient();
   async function googleSignIn() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
